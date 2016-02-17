0329eb2ad2273531e50c27666fa6f0ff64552f01
Tobias Schroepf <tobias.schroepf@jambit.com>
2016-02-17 10:56:49 +0100

    Can read heart rate sensor data

--- FeuerMoni/wear/src/main/java/com/jambit/feuermoni/MainActivity.java
+++ FeuerMoni/wear/src/main/java/com/jambit/feuermoni/MainActivity.java
@@ -1,16 +1,29 @@
 package com.jambit.feuermoni;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.util.List;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private TextView mTextView;
 
+    private Sensor heartRateSensor;
+    private SensorManager sensorManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("DUPA", "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
@@ -20,5 +33,45 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        sensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+        heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        if(heartRateSensor == null) {
+            Log.d("DUPA", "null");
+            Log.d(TAG, "heart rate sensor is null");
+
+            List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+            for (Sensor aSensor : sensors) {
+                Log.d(TAG, aSensor.getName() + ": " + aSensor.getStringType());
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        sensorManager.registerListener(this, this.heartRateSensor, 3);
+        Log.d("DUPA", "onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.values[0] > 0){
+            mTextView.setText("Rate: " + sensorEvent.values[0]);
+            Log.d(TAG, "sensor event: " + sensorEvent.accuracy + " = " + sensorEvent.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "accuracy changed: " + accuracy);
     }
 }
