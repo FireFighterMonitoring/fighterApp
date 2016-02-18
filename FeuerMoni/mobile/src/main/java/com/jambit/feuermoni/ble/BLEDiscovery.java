e7ce926ae57cb354ccf4c459d57fe56bb71dc0dc
Tobias Schroepf <tobias.schroepf@jambit.com>
2016-02-18 16:25:37 +0100

    Detect heart rate devices

--- FeuerMoni/mobile/src/main/java/com/jambit/feuermoni/ble/BLEDiscovery.java
+++ FeuerMoni/mobile/src/main/java/com/jambit/feuermoni/ble/BLEDiscovery.java
@@ -4,14 +4,20 @@ import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanRecord;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Handler;
+import android.os.ParcelUuid;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -19,7 +25,11 @@ import android.widget.Toast;
 
 import com.jambit.feuermoni.util.BackgroundThread;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by tschroep on 18.02.16.
@@ -34,8 +44,51 @@ public class BLEDiscovery {
     private final BluetoothAdapter bluetoothAdapter;
     private final BluetoothLeScanner bluetoothLeScanner;
 
+    private Context context;
+
     private boolean scanning;
     private BackgroundThread backgroundThread = new BackgroundThread();
+
+    public static String HEART_RATE_SERVICE = "0000180d-0000-1000-8000-00805f9b34fb";
+    private UUID HEART_RATE_SERVICE_UUID = UUID.fromString(HEART_RATE_SERVICE);
+
+    private Set<BluetoothDevice> compatibleScanRecords;
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            String intentAction;
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "Connected to GATT server.");
+                Log.i(TAG, "Attempting to start service discovery:" + gatt.discoverServices());
+
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "Disconnected from GATT server.");
+            }
+        }
+
+        @Override
+        // New services discovered
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                for (BluetoothGattService service : gatt.getServices()) {
+                    Log.d(TAG, "onServicesDiscovered() - Service: " + service.getUuid());
+                }
+            } else {
+                Log.w(TAG, "onServicesDiscovered received: " + status);
+            }
+        }
+
+        @Override
+        // Result of a characteristic read operation
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d(TAG, "onCharacteristicRead()");
+            }
+        }
+    };
 
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
@@ -44,6 +97,8 @@ public class BLEDiscovery {
         bluetoothManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
         bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        this.context = context;
     }
 
     public boolean isBluetoothAvailable() {
@@ -64,12 +119,7 @@ public class BLEDiscovery {
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-                Toast.makeText(activity, "BLA", Toast.LENGTH_LONG).show();
+                Toast.makeText(activity, "I need location permission!", Toast.LENGTH_LONG).show();
 
             } else {
 
@@ -78,10 +128,6 @@ public class BLEDiscovery {
                 ActivityCompat.requestPermissions(activity,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         REQUEST_LOACTION_PERMISSION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
 
             return false;
@@ -91,18 +137,32 @@ public class BLEDiscovery {
     }
 
     public void scan(final boolean enable) {
-
         final ScanCallback scanCallback = new ScanCallback() {
             @Override
             public void onScanResult(int callbackType, ScanResult result) {
                 super.onScanResult(callbackType, result);
 
                 Log.d(TAG, "onScanResult() : " + result.getDevice().getName());
-            }
 
-            @Override
-            public void onBatchScanResults(List<ScanResult> results) {
-                super.onBatchScanResults(results);
+                ScanRecord scanRecord = result.getScanRecord();
+                List<ParcelUuid> uuids = scanRecord.getServiceUuids();
+
+                if (uuids == null) {
+                    Log.d(TAG, "UUIDS are null");
+                    return;
+                }
+
+                for (ParcelUuid uuid : uuids) {
+                    Log.d(TAG, " - service uuid: " + uuid.toString());
+
+                    if (uuid.getUuid().equals(HEART_RATE_SERVICE_UUID)) {
+                        BluetoothDevice device = result.getDevice();
+                        Log.i(TAG, "Found a Heart Rate Service on device: " + device.getName());
+                        compatibleScanRecords.add(device);
+
+                        Log.d(TAG, "Set has: " + compatibleScanRecords.size() + " item(s)");
+                    }
+                }
             }
 
             @Override
@@ -112,6 +172,8 @@ public class BLEDiscovery {
         };
 
         if (enable) {
+            compatibleScanRecords = new HashSet<>();
+
             // Stops scanning after a pre-defined scan period.
             backgroundThread.getHandler().postDelayed(new Runnable() {
                 @Override
